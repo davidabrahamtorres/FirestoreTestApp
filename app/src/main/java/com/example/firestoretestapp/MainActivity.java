ee45c5db9d80b4a7522ffb30756bc389944cb636
David Abraham Torres Campos <davidabraham@google.com>
2019-01-31 17:39:19 -0600

    Initial commit

---
+++ app/src/main/java/com/example/firestoretestapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.firestoretestapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseFirestore db;
+    private TextView textDisplay;
+    private static final String TAG = "DocSnippets";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+        ReadSingleContact();
+        textDisplay = findViewById(R.id.textDisplay);
+        ReadSingleContact();
+
+    }
+
+    private void ReadSingleContact() {
+        DocumentReference docRef = db.collection("region").document("region one");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    StringBuilder fields = new StringBuilder("");
+                    fields.append("test: ").append(document.get("test"));
+                    if (document.exists()) {
+                        textDisplay.setText(fields.toString());
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+}
